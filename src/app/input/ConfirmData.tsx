465c503a7473d9fb3ecf65076db3bbbdca054bcb
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-06-07 03:39:12 +0800

    Update ConfirmData.tsx

--- src/app/input/ConfirmData.tsx
+++ src/app/input/ConfirmData.tsx
@@ -1,220 +1,230 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState, useRef } from 'react';
 import {
-	Box,
-	Button,
-	Typography,
-	Grid,
-	TextField,
-	FormGroup,
-	FormControlLabel,
-	Checkbox,
+  Box,
+  Button,
+  Typography,
+  Grid,
+  TextField,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  NativeSelect,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-
-const authorInput = 'authorInput';
-const titleInput = 'titleInput';
-const yearInput = '2000';
-const doiInput = 'doiInput';
-
 const useStyles = makeStyles((theme) => ({
-	spacing: {
-		margin: "5px"
-	},
-	crystalType: {
-		marginTop: theme.spacing(5),
-	},
-	grid2: {
-		// marginLeft: theme.spacing(1)
-	},
-	gridBtn: {
-		marginTop: theme.spacing(3),
-	},
-	chooseFiles: {},
-	submitBtn: {
-		// marginLeft: theme.spacing(30)
-	},
+  spacing: {
+    margin: '5px',
+  },
+  crystalType: {
+    marginTop: theme.spacing(5),
+  },
+  grid2: {
+    // marginLeft: theme.spacing(1)
+  },
+  gridBtn: {
+    marginTop: theme.spacing(3),
+  },
+  chooseFiles: {},
+  submitBtn: {
+    // marginLeft: theme.spacing(30)
+  },
 }));
 
 type ConfirmDataProps = {
-	step: number,
-	activeStep: number,
-	jsonData: any,
-	setJsonData: (newInput: any) => any,
+  step: number;
+  activeStep: number;
+  jsonData: any;
+  setJsonData: (newInput: any) => any;
 };
 
 export default function ConfirmData(props: ConfirmDataProps) {
-	const { step, activeStep, jsonData } = props;
+  const { step, activeStep, jsonData } = props;
 
-	const classes = useStyles();
-	const [state, setState] = React.useState({
-		checkedA: true,
-		checkedB: false,
-		checkedC: false,
-	});
-	const [crystalNameInput, setName] = React.useState('');
-	const [mineralNameInput, setMineralNameInput] = React.useState('');
-	const [volcanoNameInput, setVolcanoNameInput] = React.useState('');
-	const [eruptionYearInput, setEruptionYearInput] = React.useState('');
-	const [referenceInput, setReferenceInput] = React.useState('');
-	const [typeofCrystal, setType] = React.useState('');
-	const [author, setAuthor] = React.useState('');
-	const [title, setTitle] = React.useState('');
-	const [year, setYear] = React.useState('');
-	const [doi, setDOI] = React.useState('');
-	const formRef = React.useRef(null);
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: false,
+    checkedC: false,
+  });
+  const [crystalNameInput, setCrystalName] = React.useState('');
+  const [mineralNameInput, setMineralNameInput] = React.useState('');
+  const [volcanoNameInput, setVolcanoNameInput] = React.useState('');
+  const [eruptionYearInput, setEruptionYearInput] = React.useState('');
+  const [referenceInput, setReferenceInput] = React.useState('');
+  const [typeTraverse, setType] = React.useState('');
+  const [authorInput, setAuthor] = React.useState('');
+  //   const [reference, setReference] = React.useState('');
+  const [researchTitle, setResearchTitle] = React.useState('');
+  const [doiInput, setDOI] = React.useState('');
+  const formRef = React.useRef(null);
 
-	const handleChange = (event: any) => {
-		let name = event.target.name;
-		let value = event.target.value;
-		if (name === crystalNameInput) {
-			// setName(event.target.value);
-			props.setJsonData({ ...jsonData, "crystal name": value })
-		}
-		//else if (name === typeOfCrystalInput) {
-		//     setType(event.target.value);
-		//}
-		else if (name === authorInput) {
-			setAuthor(event.target.value);
-		} else if (name === titleInput) {
-			setTitle(event.target.value);
-		} else if (name === yearInput) {
-			setYear(event.target.value);
-		} else if (name === doiInput) {
-			setDOI(event.target.value);
-		}
-	};
+  const handleChange = (event: any) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    if (name === 'crystalNameInput') {
+      props.setJsonData({ ...jsonData, 'crystal name': value });
+      setCrystalName(value);
+    } else if (name === 'typeTraverse') {
+      props.setJsonData({ ...jsonData, 'type traverse': value });
+      setType(value);
+    } else if (name === 'mineralNameInput') {
+      props.setJsonData({ ...jsonData, mineral: value });
+      setMineralNameInput(value);
+    } else if (name === 'volcanoNameInput') {
+      props.setJsonData({ ...jsonData, volcano: value });
+      setVolcanoNameInput(value);
+    } else if (name === 'eruptionYearInput') {
+      props.setJsonData({ ...jsonData, eruption: value });
+      setEruptionYearInput(value);
+    } else if (name === 'referenceInput') {
+      props.setJsonData({ ...jsonData, reference: value });
+      setReferenceInput(value);
+    } else if (name === 'authorInput') {
+      props.setJsonData({ ...jsonData, author: value });
+      setAuthor(value);
+    } else if (name === 'doiInput') {
+      props.setJsonData({ ...jsonData, doi: value });
+      setDOI(value);
+    }
+  };
 
-	const handleCheckBoxChange = (event: any) => {
-		setState({ ...state, [event.target.name]: event.target.checked });
-	};
+  const handleCheckBoxChange = (event: any) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
-	return (
-		<div style={activeStep === step ? { display: "block" } : { display: "none" }}>
-			<Box className={classes.spacing} display="flex">
+  return (
+    <div
+      style={activeStep === step ? { display: 'block' } : { display: 'none' }}
+    >
+      <Box className={classes.spacing} display='flex'>
+        <Grid container spacing={1}>
+          <Grid item sm>
+            <FormGroup /* className= {classes.formLabel}*/>
+              <FormControl style={{ marginBottom: 20 }}>
+                <InputLabel shrink={true}>Crystal Name</InputLabel>
+                <TextField
+                  onChange={handleChange}
+                  name='crystalNameInput'
+                  margin='normal'
+                  // id='crystal-name'
+                  value={jsonData['crystal name']}
+                ></TextField>
+              </FormControl>
+              {/* <TextField
+                onChange={handleChange}
+                name='mineralNameInput'
+                margin='normal'
+                // id='crystal-name'
+                //   label='Mineral name'
+                helperText='Mineral name'
+                value={jsonData['mineral']}
+              ></TextField> */}
+              <FormControl style={{ marginBottom: 20 }}>
+                <InputLabel>Mineral Name</InputLabel>
+                <NativeSelect name='mineralNameInput' onChange={handleChange}>
+                  <option value='Plagioclase'>Plagioclase</option>
+                  <option value='Olivine'>Olivine</option>
+                  <option value='Anorthoclase'>Anorthoclase</option>
+                </NativeSelect>
+              </FormControl>
+              <FormControl style={{ marginTop: 10 }}>
+                <InputLabel shrink={true}>Volcano Name</InputLabel>
+                <TextField
+                  onChange={handleChange}
+                  name='volcanoNameInput'
+                  margin='normal'
+                  // id='crystal-name'
+                  value={jsonData['volcano']}
+                ></TextField>
+              </FormControl>
 
-				<Grid container spacing={1}>
-					<Grid item sm>
-						<FormGroup /* className= {classes.formLabel}*/>
-							<TextField
-								onChange={handleChange}
-								name={crystalNameInput}
-								margin='normal'
-								id='crystal-name'
-								//label='Crystal name'
-								helperText='Crystal name'
-								value={jsonData["crystal name"]}
-							></TextField>
-							<TextField
-								onChange={handleChange}
-								name={mineralNameInput}
-								margin='normal'
-								id='crystal-name'
-								//   label='Mineral name'
-								helperText='Mineral name'
-								value={jsonData["mineral"]}
-							></TextField>
-							<TextField
-								onChange={handleChange}
-								name={volcanoNameInput}
-								margin='normal'
-								id='crystal-name'
-								//label='Volcano name'
-								helperText='Volcano name'
-								value={jsonData["volcano"]}
-							></TextField>
+              <Typography className={classes.crystalType}>
+                Type traverse
+              </Typography>
 
-							<Typography className={classes.crystalType}>
-								Type traverse
-							</Typography>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={state.checkedA}
+                    onChange={handleCheckBoxChange}
+                    name='checkedA'
+                    color='primary'
+                  />
+                }
+                label='R-to-R'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={state.checkedB}
+                    onChange={handleCheckBoxChange}
+                    name='checkedB'
+                    color='primary'
+                  />
+                }
+                label='R-to-C'
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={state.checkedC}
+                    onChange={handleCheckBoxChange}
+                    name='checkedC'
+                    color='primary'
+                  />
+                }
+                label='C-to-C'
+              />
+            </FormGroup>
+          </Grid>
 
-							<FormControlLabel
-								control={
-									<Checkbox
-										checked={state.checkedA}
-										onChange={handleCheckBoxChange}
-										name='checkedA'
-										color='primary'
-									/>
-								}
-								label='R-to-R'
-							/>
-							<FormControlLabel
-								control={
-									<Checkbox
-										checked={state.checkedB}
-										onChange={handleCheckBoxChange}
-										name='checkedB'
-										color='primary'
-									/>
-								}
-								label='R-to-C'
-							/>
-							<FormControlLabel
-								control={
-									<Checkbox
-										checked={state.checkedC}
-										onChange={handleCheckBoxChange}
-										name='checkedC'
-										color='primary'
-									/>
-								}
-								label='C-to-C'
-							/>
-						</FormGroup>
-					</Grid>
+          <Grid item sm className={classes.grid2}>
+            <FormGroup>
+              <FormControl style={{ marginBottom: 20 }}>
+                <InputLabel shrink={true}>Eruption Year</InputLabel>
+                <TextField
+                  onChange={handleChange}
+                  name='eruptionYearInput'
+                  margin='normal'
+                  // id='crystal-name'
+                  value={jsonData['eruption']}
+                ></TextField>
+              </FormControl>
+              <FormControl style={{ marginBottom: 6 }}>
+                <InputLabel shrink={true}>Reference</InputLabel>
+                <TextField
+                  onChange={handleChange}
+                  name='referenceInput'
+                  fullWidth
+                  margin='normal'
+                  // id='author-name'
+                  value={jsonData['reference']}
+                ></TextField>
+              </FormControl>
 
-					<Grid item sm className={classes.grid2}>
-						<FormGroup>
-							<TextField
-								// onChange={handleChange}
-								// name={eruptionYearInput}
-								margin='normal'
-								id='crystal-name'
-								//label={props.eruptionYear}
-								helperText='Eruption year'
-								value={jsonData["eruption"]}
-							></TextField>
-							<TextField
-								onChange={handleChange}
-								name={referenceInput}
-								fullWidth
-								margin='normal'
-								id='author-name'
-								//label='Reference'
-								helperText='Reference'
-								value={jsonData["reference"]}
-							></TextField>
-
-							<TextField
-								onChange={handleChange}
-								name={titleInput}
-								fullWidth
-								margin='normal'
-								id='crystal-name'
-								label='Research Title'
-							></TextField>
-							<TextField
-								onChange={handleChange}
-								name={yearInput}
-								fullWidth
-								margin='normal'
-								id='year-publish'
-								label='Published year'
-							></TextField>
-							<TextField
-								onChange={handleChange}
-								name={doiInput}
-								fullWidth
-								margin='normal'
-								id='doi'
-								label='Digital Object Identifier'
-							></TextField>
-						</FormGroup>
-					</Grid>
-				</Grid>
-
-			</Box>
-		</div>
-	);
+              <TextField
+                onChange={handleChange}
+                name='researchTitle'
+                fullWidth
+                margin='normal'
+                // id='crystal-name'
+                label='Research Title'
+              ></TextField>
+              <TextField
+                onChange={handleChange}
+                name='doiInput'
+                fullWidth
+                margin='normal'
+                // id='doi'
+                label='Digital Object Identifier'
+              ></TextField>
+            </FormGroup>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 }
