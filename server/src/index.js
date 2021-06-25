9e2ac80433f016979064b3813a41139800367b93
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-06-25 15:00:55 +0800

    Search function with two or more keywords. Hardcoded.

--- server/src/index.js
+++ server/src/index.js
@@ -34,17 +34,86 @@ app.get('/crystals/:id', async (req, res) => {
 });
 
 app.get('/search', async (req, res) => {
-  const keyword = req.query.q;
+  // const keyword = req.query.q;
+  let keyword = '';
+  let keyword2 = '';
+  let keyword3 = '';
+  let keyword4 = '';
+  let keyword5 = '';
+
+  if (req.query.q != undefined) {
+    keyword = req.query.q;
+  }
+  if (req.query.q2 != undefined) {
+    keyword2 = req.query.q2;
+  }
+  if (req.query.q3 != undefined) {
+    keyword3 = req.query.q3;
+  }
+  if (req.query.q4 != undefined) {
+    keyword4 = req.query.q4;
+  }
+  if (req.query.q5 != undefined) {
+    keyword5 = req.query.q5;
+  }
   try {
     const crystal = await Crystal.find({
-      $or: [
-        { 'crystal name': { $regex: keyword, $options: 'i' } },
-        { mineral: { $regex: keyword, $options: 'i' } },
-        { volcano: { $regex: keyword, $options: 'i' } },
-        { eruption: { $regex: keyword, $options: 'i' } },
-        { mineral: { $regex: keyword, $options: 'i' } },
-        { 'type traverse': { $regex: keyword, $options: 'i' } },
-        { orientation: { $regex: keyword, $options: 'i' } },
+      $and: [
+        {
+          $or: [
+            { 'crystal name': { $regex: keyword, $options: 'i' } },
+            { mineral: { $regex: keyword, $options: 'i' } },
+            { volcano: { $regex: keyword, $options: 'i' } },
+            { eruption: { $regex: keyword, $options: 'i' } },
+            { mineral: { $regex: keyword, $options: 'i' } },
+            { 'type traverse': { $regex: keyword, $options: 'i' } },
+            { orientation: { $regex: keyword, $options: 'i' } },
+          ],
+        },
+        {
+          $or: [
+            { 'crystal name': { $regex: keyword2, $options: 'i' } },
+            { mineral: { $regex: keyword2, $options: 'i' } },
+            { volcano: { $regex: keyword2, $options: 'i' } },
+            { eruption: { $regex: keyword2, $options: 'i' } },
+            { mineral: { $regex: keyword2, $options: 'i' } },
+            { 'type traverse': { $regex: keyword2, $options: 'i' } },
+            { orientation: { $regex: keyword2, $options: 'i' } },
+          ],
+        },
+        {
+          $or: [
+            { 'crystal name': { $regex: keyword3, $options: 'i' } },
+            { mineral: { $regex: keyword3, $options: 'i' } },
+            { volcano: { $regex: keyword3, $options: 'i' } },
+            { eruption: { $regex: keyword3, $options: 'i' } },
+            { mineral: { $regex: keyword3, $options: 'i' } },
+            { 'type traverse': { $regex: keyword3, $options: 'i' } },
+            { orientation: { $regex: keyword3, $options: 'i' } },
+          ],
+        },
+        {
+          $or: [
+            { 'crystal name': { $regex: keyword4, $options: 'i' } },
+            { mineral: { $regex: keyword4, $options: 'i' } },
+            { volcano: { $regex: keyword4, $options: 'i' } },
+            { eruption: { $regex: keyword4, $options: 'i' } },
+            { mineral: { $regex: keyword4, $options: 'i' } },
+            { 'type traverse': { $regex: keyword4, $options: 'i' } },
+            { orientation: { $regex: keyword4, $options: 'i' } },
+          ],
+        },
+        {
+          $or: [
+            { 'crystal name': { $regex: keyword5, $options: 'i' } },
+            { mineral: { $regex: keyword5, $options: 'i' } },
+            { volcano: { $regex: keyword5, $options: 'i' } },
+            { eruption: { $regex: keyword5, $options: 'i' } },
+            { mineral: { $regex: keyword5, $options: 'i' } },
+            { 'type traverse': { $regex: keyword5, $options: 'i' } },
+            { orientation: { $regex: keyword5, $options: 'i' } },
+          ],
+        },
       ],
     });
     if (!crystal) {
