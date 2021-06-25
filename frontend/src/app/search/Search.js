c44cd8a133a4d7b83e6800ad6443c74c0ff072a7
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-06-25 15:05:47 +0800

    Render a loader when searching
    Yet to handle errors (when no result found)

--- frontend/src/app/search/Search.js
+++ frontend/src/app/search/Search.js
@@ -28,11 +28,39 @@ function Search() {
 
   const handleSubmit = (event) => {
     const value = event.target.value;
+    setButtonClicked(true);
     event.preventDefault();
     setSubmitText(value);
     console.log(textInput);
 
-    const url = '/search?q=' + textInput.toString();
+    let arraySearch = [];
+    let multipleInput = '';
+
+    if (textInput.includes(' and ')) {
+      arraySearch = textInput.split(' and ');
+      multipleInput = arraySearch.join('&');
+      console.log(multipleInput);
+    }
+    let url =
+      '/search?q=' +
+      arraySearch[0].toString().split(' ').join('+') +
+      (arraySearch[1]
+        ? '&q2=' + arraySearch[1].toString().split(' ').join('+')
+        : '') +
+      (arraySearch[2]
+        ? '&q3=' + arraySearch[2].toString().split(' ').join('+')
+        : '') +
+      (arraySearch[3]
+        ? '&q4=' + arraySearch[3].toString().split(' ').join('+')
+        : '') +
+      (arraySearch[4]
+        ? '&q5=' + arraySearch[4].toString().split(' ').join('+')
+        : '');
+    // let url = '/search?q=' + textInput.toString().split(' ').join('+');
+    // if (multipleInput != '') {
+    //   url = '/search?q=' + multipleInput.toString().split(' ').join('+');
+    // }
+    // console.log(url);
 
     const fetchData = async () => {
       try {
@@ -45,6 +73,14 @@ function Search() {
       }
     };
     fetchData();
+  };
+  const Loader = () => {
+    if (buttonClicked) {
+      return (
+        <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif' />
+      );
+    }
+    return '';
   };
 
   return (
@@ -78,36 +114,40 @@ function Search() {
       <br></br>
       <Typography style={{ paddingLeft: 30, fontSize: 15 }}>
         <i>
-          Please search with this format:<br></br>
-          "attribute_1=value_1&attribute_2=value_2&..."<br></br>
-          E.g. eruption=1997&volcano=Erebus
+          Please search with keywords seperated by "and".<br></br>
+          E.g. Erebus and 1997
         </i>
       </Typography>
       <br></br>
 
-      <Typography style ={{marginLeft: 25, paddingBottom : 20}}>
+      <Typography style={{ marginLeft: 25, paddingBottom: 20 }}>
         {searchData.length != 0 ? (
           <Typography>
+            {/* <ul>{items}</ul> */}
             <Typography
-        component='h3'
-        variant='h5'
-        align='center'
-        style={{paddingBottom: 10 }}
-      >
-        Search result
-      </Typography>
-            <div>{searchData.map(data => <div key={data.name}>
-              <ul>
-                <li>Crystal name: {data["crystal name"]}</li>
-                <li>Type traverse: {data["type traverse"]}</li>
-                <li>Mineral name: {data["mineral"]}</li>
-                <li>Eruption year: {data["eruption"]}</li>
-              </ul>
-              
-            </div>)}</div>
+              component='h3'
+              variant='h5'
+              align='center'
+              style={{ paddingBottom: 10 }}
+            >
+              Search result
+            </Typography>
+            <div>
+              {searchData.map((data) => (
+                <div key={data.name}>
+                  <ul>
+                    <li>Crystal name: {data['crystal name']}</li>
+                    <li>Type traverse: {data['type traverse']}</li>
+                    <li>Mineral name: {data['mineral']}</li>
+                    <li>Eruption year: {data['eruption']}</li>
+                  </ul>
+                </div>
+              ))}
+            </div>
           </Typography>
         ) : (
-          'No result found.'
+          // 'No result found.'
+          <Loader />
         )}
       </Typography>
     </Paper>
