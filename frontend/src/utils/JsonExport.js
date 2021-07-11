6c7c3e0cf6ea6b4b04922e15cbafb4fcd341db45
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-07-11 16:06:34 +0800

    Add Json export util

---
+++ frontend/src/utils/JsonExport.js
@@ -0,0 +1,11 @@
+export default function exportJsonFile(exportObj, exportName) {
+  var dataStr =
+    'data:text/json;charset=utf-8,' +
+    encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute('href', dataStr);
+  downloadAnchorNode.setAttribute('download', exportName + '.json');
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+} // create a new element with a tag and add attribute href and donwload to that a tag. No need to link to the server.
