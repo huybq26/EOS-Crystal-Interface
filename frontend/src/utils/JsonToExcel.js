678dbb6050466cc99808258611008e3316b5b9a4
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-07-11 16:05:27 +0800

    Update JsonToExcel.js

--- frontend/src/utils/JsonToExcel.js
+++ frontend/src/utils/JsonToExcel.js
@@ -1,26 +1,15 @@
 import * as XLSX from 'xlsx';
 
-export async function JsonToExcel(json) {
-  //   console.log(json);
-  let ws = await XLSX.utils.json_to_sheet(
-    [json[0]]
-    //         ,{
-    //     header: [
-    //       'crystal name',
-    //       'type traverse',
-    //       'axis',
-    //       'orientation',
-    //       'mineral',
-    //       'volcano',
-    //       'eruption',
-    //       'reference',
-    //       'spot location',
-    //     ],
-    //   }
-  );
+export function exportExcelFile(json) {
+  let ws = XLSX.utils.json_to_sheet([json[0]]);
   for (let i = 1; i < Object.keys(json).length; i++) {
-    XLSX.utils.sheet_add_json(ws, [json[i]], { origin: -1, skipHeader: true });
+    XLSX.utils.sheet_add_json(ws, [json[i]], {
+      origin: -1,
+      skipHeader: true,
+    });
   }
-  console.log(ws);
-  return ws;
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+  /* generate XLSX file and send to client */
+  XLSX.writeFile(wb, 'data_excel.xlsx');
 }
