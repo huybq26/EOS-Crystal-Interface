037f63038b50f41e75be2f99ab3da906f33cb2b0
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-07-12 13:32:29 +0800

    Finish converting search results to excel file

--- frontend/src/utils/JsonToExcel.js
+++ frontend/src/utils/JsonToExcel.js
@@ -1,67 +1,73 @@
 import * as XLSX from 'xlsx';
 
 export function exportExcelFile(json) {
-  // let traverses =
-  for (let i = 0; i < Object.keys(json).length; i++) {
-    delete json[i]['_id'];
-    delete json[i]['traverses'];
-  }
-  console.log(json);
-  let ws = XLSX.utils.json_to_sheet([json[0]]);
-  for (let i = 1; i < Object.keys(json).length; i++) {
-    XLSX.utils.sheet_add_json(ws, [json[i]], {
-      origin: -1,
-      skipHeader: true,
-    });
-  }
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
-  /* generate XLSX file and send to client */
-  XLSX.writeFile(wb, 'data_excel.xlsx');
-}
-
-export function exportTraverse(json) {
-  // let traverses =
   let traverses = [{}];
   for (let i = 0; i < Object.keys(json).length; i++) {
-    delete json[i]['_id'];
+    delete json[i]['_id']; // delete id column
+
     if (Object.keys(json[i]['traverses']).length > 0) {
-      traverses.unshift(json[i]['traverses']);
+      traverses.push(json[i]['traverses']);
     }
-    // delete json[i]['_id'];
+    delete json[i]['traverses']; // delete traverse column after adding to the array above
   }
-  console.log(traverses);
+
+  let ws = XLSX.utils.json_to_sheet([json[0]]); // initialize worksheet with information of the first crystal
+
+  // console.log(traverses);
   let newObj = Object.keys(traverses).reduce(function (s, a) {
     s[a] = traverses[a][Object.keys(traverses[a])];
     return s;
-  }, {});
-  //   let newObj2 = Object.keys(newObj).reduce(function (s, a) {
-  //     s[a] = newObj[a][Object.keys(newObj[a])];
-  //     return s;
-  //   }, {});
-  console.log(newObj[1]);
-  let ws = XLSX.utils.json_to_sheet([newObj[0][0]]);
-  let i = 0;
-  while (i < Object.keys(newObj).length - 1) {
-    // console.log(Object.keys(newObj[i]).length);
-    // if (newObj[i] != undefined) {
-    // console.log(Object.keys(newObj[0]).length * 10);
-    for (let j = 0; j <= 10000; j = j + 10) {
-      // depends on how we store the data. If the key is also the same as the spot location, it will be very complicated. A suggestion is to put the key as increasing natural numbers only.
-      if (i == 0 && j == 0) {
-        continue;
-      }
+  }, {}); // reduce function to simplify the data structure of traverses
+  // console.log(newObj[1]);
+
+  // Add information of the first object. Note that the newObj[0] is undefined, so start from newObj[1]
+  XLSX.utils.sheet_add_json(ws, [newObj[1][0]], {
+    origin: 'I1',
+    skipHeader: false,
+  }); // both row 1 and 2 are occupied by information and spot location 0.
+  let location_index = 3; // start from row 3
+  for (let j = 10; j <= 10000; j = j + 10) {
+    // depends on how we store the data. If the key is also the same as the spot location, it will be very complicated. A suggestion is to put the key as increasing natural numbers only.
+    let location = 'I' + location_index; // e.g.: I3
+    if (newObj[1][j]) {
+      XLSX.utils.sheet_add_json(ws, [newObj[1][j]], {
+        origin: location,
+        skipHeader: true,
+      }); // add traverses information from spot location 10 till 10000 (if applicable)
+      location_index += 1; // go down one line
+    }
+  }
+  location_index += 1;
+
+  for (let i = 2; i < Object.keys(newObj).length; i++) {
+    // add information of object 2,3,4,...
+    XLSX.utils.sheet_add_json(ws, [json[i - 1]], {
+      origin: 'A' + location_index,
+      skipHeader: true,
+    });
+
+    XLSX.utils.sheet_add_json(ws, [newObj[i][0]], {
+      origin: 'I' + location_index,
+      skipHeader: true,
+    });
+    location_index += 1;
+    for (let j = 10; j <= 10000; j = j + 10) {
+      let location = 'I' + location_index;
       if (newObj[i][j]) {
         XLSX.utils.sheet_add_json(ws, [newObj[i][j]], {
-          origin: -1,
+          origin: location,
           skipHeader: true,
         });
+        location_index += 1;
       }
     }
-    i = i + 1;
+    location_index += 1;
   }
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'Document');
-  /* generate XLSX file and send to client */
-  XLSX.writeFile(wb, 'data_excel.xlsx');
+
+  // console.log(ws);
+
+  const wb = XLSX.utils.book_new(); // new workboox
+  XLSX.utils.book_append_sheet(wb, ws, 'Document'); // append worksheet to workbook with name = Document
+  XLSX.writeFile(wb, 'data_excel.xlsx'); // generate XLSX file and send to client
+  // console.log(wb);
 }
