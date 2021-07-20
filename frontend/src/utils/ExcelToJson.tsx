6b0eca3614f442b0d84fa5a9cde62b5132cbc2dd
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-07-20 11:48:50 +0800

    Minor changes to ExcelToJson function

--- frontend/src/utils/ExcelToJson.tsx
+++ frontend/src/utils/ExcelToJson.tsx
@@ -1,56 +1,54 @@
-import * as XLSX from "xlsx";
-
+import * as XLSX from 'xlsx';
 
 export async function excelToJson(excelFile: File) {
+  const csv = await getCSVfromFile(excelFile);
+  const data = convertCSVtoJson(csv);
 
-    const csv = await getCSVfromFile(excelFile);
-    const data = convertCSVtoJson(csv);
-
-    return data[0];
+  return data[0];
 }
 
 async function getCSVfromFile(excelFile: File) {
-    return new Promise<string>((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsBinaryString(excelFile);
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(excelFile);
 
-        reader.onload = (event) => {
-            const bstr = event.target?.result;
-            const workbook = XLSX.read(bstr, { type: "binary" });
-            let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-            const csv = XLSX.utils.sheet_to_csv(worksheet);
-            resolve(csv);
-        };
+    reader.onload = (event) => {
+      const bstr = event.target?.result;
+      const workbook = XLSX.read(bstr, { type: 'binary' }); 
+      let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const csv = XLSX.utils.sheet_to_csv(worksheet, { FS: '~' }); 
+      resolve(csv);
+    };
 
-        reader.onerror = (err) => {
-            reject(err);
-        };
-    });
+    reader.onerror = (err) => {
+      reject(err);
+    };
+  });
 }
 
 function convertCSVtoJson(csv: String) {
-    let lines = csv.split("\n")
-    let json = [];
-    let headers = lines[0].split(",");
+  let lines = csv.split('\n');
+  let json = [];
+  let headers = lines[0].split('~');
 
-    let common: { [key: string]: any } = {};
-    let firstLine = lines[1].split(",")
-    for (let i = 0; i < 8; i++) {
-        common[headers[i]] = firstLine[i];
-    }
-    json.push(common);
-    json[0]["traverse"] = [];
+  let common: { [key: string]: any } = {}; 
+  let firstLine = lines[1].split('~');
+  for (let i = 0; i < 8; i++) {
+    common[headers[i]] = firstLine[i]; 
+  }
+  json.push(common);
+  json[0]['traverse'] = [];
 
-    for (let i = 1; i < lines.length; i++) {
-        let obj: { [key: string]: any } = {};
-        let currentline = lines[i].split(",");
+  for (let i = 1; i < lines.length; i++) {
+    let obj: { [key: string]: any } = {};
+    let currentline = lines[i].split('~');
 
-        for (let j = 8; j < headers.length; j++) {
-            obj[headers[j]] = currentline[j];
-        }
-
-        json[0]["traverse"].push(obj);
+    for (let j = 8; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j]; 
     }
 
-    return json;
+    json[0]['traverse'].push(obj);
+  }
+  console.log(json);
+  return json;
 }
