41eea722afea274d26d8c927b5a979f2249405d0
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-08-06 20:35:25 +0800

    Fix dependency errors

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 # EOS-Crystal-Interface
 
-Please make sure you have connected to EOS MongoDB database in localhost. You can also copy the EOS database to your local MongoDB.
+Please make sure you have connected to EOS MongoDB database in localhost.
 
-Change the port in server/src/db/mongoose.js to the one you are using. The current port is 27017 (mongodb://127.0.0.1:27017/eos).
+Change the port in server/src/db/mongoose.js to the one you are using. The current port is 41569 (mongodb://127.0.0.1:41569/eos).
 
 In the project directory, you need to open two terminals and run these lines of code:
 
 Terminal 1: (server)
-### `cd server` 
+
+### `cd server`
+
 ### `npm install` (just need to run in the first time)
+
 ### `npm start`
+
 The server will run in port 5000 (http://localhost:5000)
 
 Terminal 2: (frontend)
-### `cd frontend` 
+
+### `npm install`
+
+### `cd frontend`
+
 ### `npm install` (just need to run in the first time)
+
 ### `npm start`
 
 To run the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.\
-
-Note: If you want to use local MongoDB (using port 27017) instead of online MongoDB (using another port), run this command line beforehand:
-
-### `sudo systemctl start mongod.service`
--- server/server.js
+++ server/server.js
@@ -7,31 +7,48 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   cors = require('cors'),
   mongoose = require('mongoose'),
-  config = require('./config/DB'),
+  config = require('./config/DB');
 const { constants } = require('crypto');
 const fs = require('fs');
 const crystal = require('./models/crystal');
 
 // api config
+const app = express();
 app.use('/crystal', crystalRoutes);
 
 // mongoose config
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database' + err) }
-);
+mongoose
+  .connect(config.DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log('Database is connected');
+    },
+    (err) => {
+      console.log('Can not connect to the database' + err);
+    }
+  );
 
 // CORS handle
-const app = express();
+// const app = express();
 // app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 app.use(cors());
 const port = process.env.PORT || 5000;
 
 // Mapping Express Route with Server Route
-
 
 // Run NOTE: This one for development
 const server = app.listen(port, function () {
