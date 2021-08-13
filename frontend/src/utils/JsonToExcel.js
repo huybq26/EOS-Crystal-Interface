d83d2a01137f223743080e4e88f7308603ea308a
Quang Huy <81632425+huybq26@users.noreply.github.com>
2021-08-13 17:33:49 +0800

    Update JsonToExcel.js

--- frontend/src/utils/JsonToExcel.js
+++ frontend/src/utils/JsonToExcel.js
@@ -13,12 +13,13 @@ export function exportExcelFile(json) {
 
   let ws = XLSX.utils.json_to_sheet([json[0]]); // initialize worksheet with information of the first crystal
 
-  // console.log(traverses);
+  console.log(traverses);
   let newObj = Object.keys(traverses).reduce(function (s, a) {
     s[a] = traverses[a][Object.keys(traverses[a])];
     return s;
   }, {}); // reduce function to simplify the data structure of traverses
-  // console.log(newObj[1]);
+  console.log(newObj[1]);
+  console.log(newObj[1][1]);
 
   // Add information of the first object. Note that the newObj[0] is undefined, so start from newObj[1]
   XLSX.utils.sheet_add_json(ws, [newObj[1][0]], {
@@ -26,7 +27,7 @@ export function exportExcelFile(json) {
     skipHeader: false,
   }); // both row 1 and 2 are occupied by information and spot location 0.
   let location_index = 3; // start from row 3
-  for (let j = 10; j <= 10000; j = j + 10) {
+  for (let j = 1; j <= 1000; j = j + 1) {
     // depends on how we store the data. If the key is also the same as the spot location, it will be very complicated. A suggestion is to put the key as increasing natural numbers only.
     let location = 'I' + location_index; // e.g.: I3
     if (newObj[1][j]) {
@@ -37,7 +38,7 @@ export function exportExcelFile(json) {
       location_index += 1; // go down one line
     }
   }
-  location_index += 1;
+  // location_index += 1;
 
   for (let i = 2; i < Object.keys(newObj).length; i++) {
     // add information of object 2,3,4,...
@@ -51,7 +52,7 @@ export function exportExcelFile(json) {
       skipHeader: true,
     });
     location_index += 1;
-    for (let j = 10; j <= 10000; j = j + 10) {
+    for (let j = 1; j <= 1000; j = j + 1) {
       let location = 'I' + location_index;
       if (newObj[i][j]) {
         XLSX.utils.sheet_add_json(ws, [newObj[i][j]], {
@@ -64,7 +65,7 @@ export function exportExcelFile(json) {
     location_index += 1;
   }
 
-  // console.log(ws);
+  // // console.log(ws);
 
   const wb = XLSX.utils.book_new(); // new workboox
   XLSX.utils.book_append_sheet(wb, ws, 'Document'); // append worksheet to workbook with name = Document
